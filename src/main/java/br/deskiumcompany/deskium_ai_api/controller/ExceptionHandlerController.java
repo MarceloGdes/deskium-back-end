57a03be72e3488217d0af5082437c4d7f3bad071
Marcelo <guedesmarcello733@outlook.com>
2025-10-11 20:34:02 -0300

    feat: ExceptionHandlerController

---
+++ src/main/java/br/deskiumcompany/deskium_ai_api/controller/ExceptionHandlerController.java
@@ -0,0 +1,62 @@
+package br.deskiumcompany.deskium_ai_api.controller;
+
+import br.deskiumcompany.deskium_ai_api.dto.exception.ApiExceptionDTO;
+import br.deskiumcompany.deskium_ai_api.exception.BussinesException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiExceptionDTO> handleException(Exception e){
+        ApiExceptionDTO apiExceptionDto = new ApiExceptionDTO("Erro interno no Servidor. Contate o Suporte.");
+
+        return ResponseEntity.internalServerError().body(apiExceptionDto);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiExceptionDTO> handleApiExceptionDtoResponseEntity(
+            MethodArgumentNotValidException e){
+
+        List<String> errors = new ArrayList<>();
+
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()){
+            errors.add(fieldError.getField() + ": " + fieldError.getDefaultMessage());
+        }
+
+        ApiExceptionDTO apiExceptionDto = new ApiExceptionDTO(errors);
+        return ResponseEntity.badRequest().body(apiExceptionDto);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ApiExceptionDTO> handleEntityNotFoundException(EntityNotFoundException e){
+        ApiExceptionDTO apiExceptionDTO = new ApiExceptionDTO(e.getMessage());
+
+        //notFound do response entity não aceita body.
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiExceptionDTO);
+    }
+
+    @ExceptionHandler(BussinesException.class)
+    public ResponseEntity<ApiExceptionDTO> handleBussinesException(BussinesException e){
+        ApiExceptionDTO apiExceptionDTO = new ApiExceptionDTO(e.getMessage());
+
+        return ResponseEntity.badRequest().body(apiExceptionDTO);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ApiExceptionDTO> handleBadCredentialsException(BadCredentialsException e){
+        ApiExceptionDTO apiExceptionDTO = new ApiExceptionDTO(e.getMessage());
+
+        return ResponseEntity.badRequest().body(apiExceptionDTO);
+    }
+}
---
+++ src/main/java/br/deskiumcompany/deskium_ai_api/exception/BussinesException.java
@@ -0,0 +1,7 @@
+package br.deskiumcompany.deskium_ai_api.exception;
+
+public class BussinesException extends Exception{
+    public BussinesException(String message) {
+        super(message);
+    }
+}
