22f9e85a8fcabd0d441a43cd033a96fe988a28b2
Marcelo <guedesmarcello733@outlook.com>
2025-11-06 19:40:21 -0300

    update: adição de SubStatus

--- src/main/java/br/deskiumcompany/deskium_ai_api/domain/enums/SubStatus.java
+++ src/main/java/br/deskiumcompany/deskium_ai_api/domain/enums/SubStatus.java
@@ -3,7 +3,8 @@ package br.deskiumcompany.deskium_ai_api.domain.enums;
 public enum SubStatus {
     NOVO("Novo"),
     EM_ATENDIMENTO("Em atendimento"),
-    AGUARDANDO_RETORNO("Aguardando retono");
+    AGUARDANDO_RETORNO("Aguardando retono"),
+    FECHADO("Fechado");
 
     private final String desc;
 
